372c90a905f3d084fd82f1bf6499b85ad28b487a
Denis-Andrei <denispeptanariu@gmail.com>
2020-12-08 13:47:37 +0000

    structure the client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,25 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getMessage } from "./service";
 import logo from "./logo.svg";
+import { Route } from "react-router-dom";
+import Filter from './components/Filter/Filter';
+
 
 export function App() {
-	const [message, setMessage] = useState("Loading...");
+	const [message, setMessage] = useState("Loading.........");
 
-	useEffect(() => {
-		getMessage().then((message) => setMessage(message));
-	}, []);
+	// useEffect(() => {
+	// 	getMessage().then((message) => setMessage(message));
+	// }, []);
 
 	return (
 		<main role="main">
 			<div>
 				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
-				<h1 className="message" data-qa="message">{message}</h1>
+				<h1 className="message" data-qa="message">fdfjdsjkfhdsgasdfgdf</h1>
 			</div>
+
+			<Filter />
 		</main>
 	);
 }
