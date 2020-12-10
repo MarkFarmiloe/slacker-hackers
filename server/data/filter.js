cdcddf60fbda154648a7dd88f5900af6fd6c8d5d
Leroy Douglas <leroi.douglas@gmail.com>
2020-12-10 16:35:26 +0000

    changed data from filter.json to filter.js

---
+++ server/data/filter.js
@@ -0,0 +1,9 @@
+
+const filter = {
+	"locations": [ "Birmingham", "London", "Manchester", "Glasgow" ],
+	"classes": [ "West Midlands class 1",  "West Midlands class 2", "London class 1", "London class 2"],
+	"performance": [ "Good", "Average", "Poor"],
+};
+
+
+export default filter;
