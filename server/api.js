ca30375ac72816065d2ff2913dc29d42d7b799de
Leroy Douglas <leroi.douglas@gmail.com>
2020-12-11 10:24:49 +0000

    Changed to resolve a timeout issue. The DB connections were not being released

--- server/api.js
+++ server/api.js
@@ -2,74 +2,124 @@
 import { Router } from "express";
 
 import { Connection } from "./db";
+import sql from "sql";
 import filter from "./data/filter";
 import testJSON from "./data/testPerformance";
 
 const router = new Router();
 
 router.get("/", (_, res, next) => {
-
 	Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
 		res.json({ message: "Hello, world!" });
+
+	});
+});
+
+router.get("/create/perform", (_, res, next) => {
+	const usersToInsert = testJSON;
+	const  performSchema = sql.define({
+		name: "perform",
+		columns: [
+			"id",
+			"name",
+			"posts",
+			"week",
+			"location",
+			"class",
+			"performance",
+		],
+	});
+
+	Connection.connect((err, client, release) => {
+		if (err) {
+			return next(err);
+		}
+
+		const insertQuery = performSchema.insert(usersToInsert).returning(performSchema.id).toQuery();
+
+		client.query(insertQuery)
+			.then((result) => {
+				if(result.rowCount){
+					res.send("Updated the perform table successfully");
+				} else {
+					res.send("No Updates to the  perform table were applied");
+				}
+				release();
+			}).catch((e) => console.log(e));
 	});
 });
 
 router.get("/perform", (_, res, next) => {
+	const selectQuery = "SELECT * FROM perform";
 
-	Connection.connect((err) => {
+	Connection.connect((err, client, release) => {
 		if (err) {
+			res.status(400).json( { message: "Connection issue. Check the server logs" });
 			return next(err);
 		}
-		console.log("responded to route /perform");
-		res.json(testJSON);
+		client.query(selectQuery)
+			.then((result) => {
+				if(result.rowCount){
+					res.json(result.rows);
+				} else {
+					res.status(404).send("Could not find any statistics in the  perform table");
+				}
+
+				release();
+			}).catch((e) => console.log(e));
 	});
 });
 
 router.get("/filter", (_, res, next) => {
 
-	Connection.connect((err) => {
+	/* Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
 		console.log("responded to route /filter");
 		res.json(filter);
-	});
+	}); */
+	res.json(filter);
 });
 
 router.get("/filter/locations", (_, res, next) => {
 
-	Connection.connect((err) => {
+	/* Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
 		console.log("responded to route /search/criteria");
 		res.json(filter.locations);
-	});
+	}); */
+	res.json(filter.locations);
 });
 
 router.get("/filter/classes", (_, res, next) => {
 
-	Connection.connect((err) => {
+	/* Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
 		console.log("responded to route /search/criteria");
 		res.json(filter.classes);
-	});
+	}); */
+	res.json(filter.classes);
 });
 
 router.get("/filter/performance", (_, res, next) => {
 
-	Connection.connect((err) => {
+	/* Connection.connect((err) => {
 		if (err) {
 			return next(err);
 		}
 		console.log("responded to route /search/criteria");
 		res.json(filter.performance);
-	});
+	}); */
+	console.log("responded to route /search/criteria");
+	res.json(filter.performance);
 });
 
 router.get("/test", (_, res, next) => {
