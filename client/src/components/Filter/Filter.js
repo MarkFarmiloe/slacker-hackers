d13a6533ab6419d48481580326183117260559d3
Denis-Andrei <denispeptanariu@gmail.com>
2020-12-12 16:02:15 +0000

    Filter component modified to use fetched data

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,53 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import './filter.css'
 
-const locationArr = ['Birmingham','London','Manchester','Glasgow'];
-const classArr = ['WestMidlands Class 1','WestMidlands Class 2','London Class 1','London Class 2'];
-const performanceArr = ['Good','Average','Poor'];
-
-const data ={
-    "locations": [
-        {
-            "city": "Birmingham",
-            "classes": [
-                "West Midlands class 1",
-                "West Midlands class 2"
-            ]
-        },
-        {
-            "city": "London",
-            "classes": [
-                "London class 1",
-                "London class 2"
-            ]
-        },
-        {
-            "city": "Manchester",
-            "classes": [
-                "Manchester class 1"
-            ]
-        },
-        {
-            "city": "Glasgow",
-            "classes": [
-                "Glasgow class 1"
-            ]
-        }
-    ],
-    "performance": [
-        "Good",
-        "Average",
-        "Poor"
-    ]
-}
-
-
  function Filter(prop) {
 
+    const [data, setData] = useState({});
     const [optionLocation, setOptionLocation] = useState('');
     const [optionClass, setOptionClass] = useState('');
     const [optionPerformance, setPerformanceClass] = useState('');
     const [searchVal, setSearchVal] = useState('');
+
+
     /////////////////////////////edit by zubeda
     function filterFunc(location){
       
@@ -80,42 +42,45 @@ const data ={
       
     }
 
-    // useEffect(() => {
-        // fetch('https://slacker-hackers.herokuapp.com/api/filter')
-        // .then(res => res.json())
-        // .then(data=> console.log(data))
-        // .catch(err => console.log(err))
-    // }, [])
+    useEffect(() => {
+        fetch('https://slacker-hackers.herokuapp.com/api/filter')
+        .then(res => res.json())
+        .then(data=> setData(data))
+        .catch(err => console.log(err))
+    }, [data])
+
+    //filter the data depending what a teacher select on the Location dropdown 
     let filteredLocationObj;
-    filteredLocationObj = data.locations.filter(item => item.city == optionLocation )
-    return (
+    Object.entries(data) != 0 ? filteredLocationObj = data.locations.filter(item => item.city == optionLocation ) : '';
+
+    return Object.entries(data) != 0 ? (
         <div className='filter-section'>
                 <div className='select-box'>
                     <span>Filter by:</span>
                     <select onChange={handleLocationChange} className='select-location'>
                         <option disabled selected hidden>Location</option>
                         {
-                            // locationArr.map((location, index) => <option key={index}>{location}</option>)
-
                             data.locations.map((location, index) => <option key={index}>{location.city}</option>)
                         }
                     </select>
                     <select onChange={handleClassChange} className='select-class'>
                         <option disabled selected hidden>Class</option>
                         {
-                           filteredLocationObj.length > 0 ? 
-                           filteredLocationObj[0].classes.map((item,index) => <option key={index}>{item}</option>)
+                            filteredLocationObj.length > 0 
+                            ? 
+                            filteredLocationObj[0].classes.map((item,index) => <option key={index}>{item}</option>)
                             :
-                            ''
+                            <option disabled>Select Location First</option>
                         }
                     </select>
                     <select onChange={handlePerformanceChange} className='select-performance'>
                         <option disabled selected hidden>Performance</option>
+                        {/* edit by zubeda */}
+                        <option>Select All</option>
                         {
                             data.performance.map((performance, index) => <option key={index}>{performance}</option>)
                         }
-                        {/* edit by zubeda */}
-                        <option>Select All</option>
+                        
                     </select>
                 </div>
                 
@@ -129,6 +94,8 @@ const data ={
             
         </div>
     )
+    :
+    '';
 }
 
 export default Filter;
